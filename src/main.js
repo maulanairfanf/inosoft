bc1dfa118d5374184303cbc78a88eef536ee579f
maulanairfanf <maulanairfanf@gmail.com>
2022-12-04 21:21:03 +0700

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import jQuery from 'jquery';
+import 'bootstrap';
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/css/bootstrap.css';
+global.jQuery = jQuery;
+global.$ = jQuery;
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
