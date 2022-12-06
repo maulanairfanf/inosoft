7b75d25642a5b2f5f7e7d7e28041b054699815f9
maulanairfanf <maulanairfanf@gmail.com>
2022-12-06 14:40:32 +0700

    create component input select and text/number

---
+++ src/components/InputSelect.vue
@@ -0,0 +1,22 @@
+<template>
+  <select
+    class="bg-gray-50 border text-sm text-gray-400 rounded block p-2.5"
+    :class="width"
+  >
+    <option>Select an Option</option>
+    <option selected value="US">SHP</option>
+    <option value="CA">Canada</option>
+    <option value="FR">France</option>
+    <option value="DE">Germany</option>
+  </select>
+</template>
+
+<script>
+  export default {
+    name: 'InputSelect',
+    props: {
+      listOption: [],
+      width: String,
+    },
+  };
+</script>
---
+++ src/components/InputText.vue
@@ -0,0 +1,21 @@
+<template>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    :class="width"
+    :value="value"
+    class="bg-gray-50 border text-sm text-gray-700 rounded block p-2.5"
+  />
+</template>
+
+<script>
+  export default {
+    name: 'InputText',
+    props: {
+      placeholder: String,
+      width: String,
+      type: String,
+      value: String,
+    },
+  };
+</script>
