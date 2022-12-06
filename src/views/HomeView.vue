d2c7ba7fc0cfd6695e8c5a8936178b4c7cb29fe0
maulanairfanf <maulanairfanf@gmail.com>
2022-12-06 14:40:45 +0700

    show cost detail

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <button class="btn btn-primary">Click</button>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <CostDetail />
   </div>
 </template>
 
 <script>
   // @ is an alias to /src
   import HelloWorld from '@/components/HelloWorld.vue';
-
+  import CostDetail from '@/components/CostDetail.vue';
   export default {
     name: 'HomeView',
     components: {
       HelloWorld,
+      CostDetail,
     },
   };
 </script>
