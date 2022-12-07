33b5b3630aeb03c30f14bb6ac6ab9b745b534634
maulanairfanf <maulanairfanf@gmail.com>
2022-12-07 11:39:51 +0700

    create Information Detail (Part A)

---
+++ src/components/InformationDetail.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="border"><GridInformation /></div>
+</template>
+<script>
+  import GridInformation from './Grid/GridInformation.vue';
+  export default {
+    name: 'InformationDetail',
+    components: { GridInformation },
+  };
+</script>
+<style lang=""></style>
